bb02bd8d38844ac9d5940447bdb22de6f786bd27
David Correia <david.cs20@gmail.com>
2024-02-10 11:41:31 -0300

    feat: Componente de busca inicial

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Header from "../_components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale/pt-BR";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, David!</h2>
         <p className="capitalize text-sm">
@@ -14,6 +16,9 @@ export default function Home() {
             locale: ptBR,
           })}
         </p>
+      </div>
+      <div className="px-5 mt-6">
+        <Search/>
       </div>
     </div>
   );
