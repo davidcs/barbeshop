28dc7539942c8fe90f99d9379bd6cdb0200e43ee
David Correia <david.cs20@gmail.com>
2024-02-11 19:05:28 -0300

    feat: footer inicial

--- src/app/(home)/_components/barbershop-item.tsx
+++ src/app/(home)/_components/barbershop-item.tsx
@@ -6,24 +6,24 @@ import { StarIcon } from "lucide-react";
 import Image from "next/image";
 
 
-interface BarbershopItemProps{
+interface BarbershopItemProps {
     barbershop: Barbershop;
 }
 
-const BarbershopItem = ({barbershop}: BarbershopItemProps) => {
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
     return (
         <Card className="min-w-[167px] max-w-[167px] rounded-2xl" >
             <CardContent className="px-1 py-0">
                 <div className="w-full h-[159px] relative">
                     <div className="absolute top-2 left-2 z-50">
-                        <Badge variant="secondary" className="opacity-90 flex items-center top-3 left-3 ">
+                        <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3 ">
                             <StarIcon size={12} className="fill-primary text-primary">
                                 <span className="text-xs">5,0</span>
                             </StarIcon>
                         </Badge>
                     </div>
 
-                    <Image alt={barbershop.name} src={barbershop.imageUrl} style={{objectFit: "cover"}} fill className=" rounded-2xl" />
+                    <Image alt={barbershop.name} src={barbershop.imageUrl} style={{ objectFit: "cover" }} fill className=" rounded-2xl" />
                 </div>
                 <div className="px-0 pb-3">
                     <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap"> {barbershop.name}</h2>
